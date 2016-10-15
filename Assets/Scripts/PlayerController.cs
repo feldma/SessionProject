3133b20b8866ab380224df48de9d928c9c75d994
Nounours <Nounours>
2016-10-15 21:21:10 +0200

    Initial commit with a simple scene with a player moving on a plane

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    float rotationSpeed = 100.0f;
+
+    [SerializeField]
+    float moveSpeed = 10.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        float move = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Translate(0, 0, move);
+        transform.Rotate(0, rotation, 0);
+	}
+}
